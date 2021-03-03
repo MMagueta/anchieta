192c7cad2272d83447c2568991bf43d8b6e00de5
Marcos Magueta <maguetamarcos@gmail.com>
2021-03-02 21:24:46 -0300

    python last update

--- Anchieta.py
+++ Anchieta.py
@@ -1,41 +1,122 @@
 import networkx as nx
 import statistics
 import numpy as np
-
-#import numpy as np
+from threading import Thread
 from AX import Parse
+from math import floor
+import time
+import sys
 
-def degrees():
-    from time import sleep
-    temporal = np.array([])
-    G = nx.read_gexf('out.gexf')
-    print(len(G.edges(data=True)))
-    timestamps = sorted(list(map(lambda item: item[2]['start'], G.edges(data=True))), key=lambda  x: x)
-    print("Timestamps sorted: {}".format(len(timestamps)))
-    for t in timestamps:
-        #filtered_edges = map(lambda x: (x[0], x[1], int(x[2]['id'])), filter(lambda item: item[2]['start'] <= t and item[2]['end'] >= t, G.edges(data=True)))
-        #print(G.edges)
-        filtered_edges = map(lambda x: (x[0], x[1], int(x[2]['id'])), (item for item in G.edges(data=True) if (item[2]['start'] <= t and item[2]['end'] >= t)))
-        H = G.edge_subgraph(list(filtered_edges))
-        H = nx.Graph(H)
-        H.add_nodes_from(G.nodes())
-        np.append(temporal, np.mean(np.array(list(nx.average_neighbor_degree(H).values()))))
-        #temporal.append(statistics.mean([val for (node, val) in H.degree()]))
-    return temporal
+start = time.time()
+#numbers = []
+G = nx.read_gexf('gexfs/'+sys.argv[1]+'.gexf')
+timestamps = sorted(set(list(map(lambda item: item[2]['start'], G.edges(data=True)))), key=lambda  x: x)
+#print(set(timestamps))
+temporal = np.zeros(len(timestamps))
+print("Edges:", len(temporal))
 
-def convert_dynamic_graph():
-    temporal = np.array([])
-    G = nx.read_gexf('out.gexf')
-    timestamps = numpy.sort(np.fromiter(map(lambda item: item[2]['start'], G.edges(data=True)), dtype=np.float))
-    for t in timestamps:
-        filtered_edges = map(lambda x: (x[0], x[1], int(x[2]['id'])), (item for item in G.edges(data=True) if (item[2]['start'] <= t and item[2]['end'] >= t)))
-        H = G.edge_subgraph(list(filtered_edges))
-        H = nx.Graph(H)
-        H.add_nodes_from(G.nodes())
-        np.append(temporal, np.mean(np.array(list(nx.average_neighbor_degree(H).values()))))
-    ##TO-DO: Make it multithreaded
-    return temporal
+def temporal_calculation(id, number_threads, fun):
+    pieces = floor(len(timestamps)/number_threads)
+    if id+1 != number_threads:
+        thread_range = [pieces*id, pieces*(id+1)-1]
+    else:
+        thread_range = [pieces*id, len(timestamps)]
+
+    print(id, thread_range)
+    for t in range(thread_range[0]-1, thread_range[1]):
+        if t > -1: #Must ignore the first because of the range initial state, eg.: [0-1=-1, 1] and [1-1=0, 2]
+            #numbers.append(t)
+            H = nx.Graph()
+            filtered_edges = list(map(lambda x: (int(x[0]), int(x[1])), (item for item in G.edges(data=True) if (item[2]['start'] <= timestamps[t] and item[2]['end'] >= timestamps[t]))))
+            #print(filtered_edges)
+            H.add_nodes_from(G.nodes())
+            H.add_edges_from(filtered_edges)
+            temporal[t] = fun(H)
+            #print(temporal[t])
 
 if __name__=="__main__":
-    #print(degrees())
-    Parse()
+    #Parse()
+    try:
+        threads = []
+        if len(temporal) < 56: #Number of cores
+            N = len(temporal)-1
+        else:
+            N = 56
+        print("N:", N)
+        for i in range(N):
+            #threads.append(Thread(target=temporal_calculation, args=( i, N, #lambda H: np.mean(np.array(list(nx.average_neighbor_degree(H).values()))) ))) #Degree
+            #threads.append(Thread(target=temporal_calculation, args=( i, N, lambda H: np.mean(list(nx.average_neighbor_degree(H).values())) )))
+            #threads.append(Thread(target=temporal_calculation, args=( i, N, lambda H: nx.average_shortest_path_length(H) )))
+            threads.append(Thread(target=temporal_calculation, args=( i, N, lambda H: nx.density(H) )))
+        for i in range(N):
+            threads[i].start()
+        for i in range(N):
+            threads[i].join()
+        #print(sorted(numbers))
+        np.savetxt('temporal/densidade/'+sys.argv[1]+'.csv', temporal, delimiter=',')
+        np.savetxt('temporal/densidade/T_'+sys.argv[1]+'.csv', timestamps, delimiter=',')
+        print(time.time() - start)
+    except Exception as e:
+        print ("Unable to start thread: {}".format(str(e)))
+
+
+"""
+
+import networkx as nx
+import statistics
+import numpy as np
+from threading import Thread
+from AX import Parse
+from math import floor
+import time
+import sys
+
+start = time.time()
+#numbers = []
+G = nx.read_gexf('gexfs/'+sys.argv[1]+'.gexf')
+timestamps = sorted(list(map(lambda item: item[2]['start'], G.edges(data=True))), key=lambda  x: x)
+temporal = np.zeros(len(timestamps))
+print("Edges:", len(temporal))
+
+def temporal_calculation(id, number_threads, fun):
+    pieces = floor(G.number_of_edges()/number_threads)
+    if id+1 != number_threads:
+        thread_range = [pieces*id, pieces*(id+1)-1]
+    else:
+        thread_range = [pieces*id, G.number_of_edges()]
+
+    #print(id, thread_range)
+    for t in range(thread_range[0]-1, thread_range[1]):
+        if t > -1: #Must ignore the first because of the range initial state, eg.: [0-1=-1, 1] and [1-1=0, 2]
+            #numbers.append(t)
+            H = nx.Graph()
+            filtered_edges = list(map(lambda x: (int(x[0]), int(x[1])), (item for item in G.edges(data=True) if (item[2]['start'] <= timestamps[t] and item[2]['end'] >= timestamps[t]))))
+            #print(filtered_edges)
+            H.add_nodes_from(G.nodes())
+            H.add_edges_from(filtered_edges)
+            temporal[t] = fun(H)
+            #print(temporal[t])
+
+if __name__=="__main__":
+    #Parse()
+    try:
+        threads = []
+        if len(temporal) < 48: #Number of cores
+            N = len(temporal)-1
+        else:
+            N = 48
+        print("N:", N)
+        for i in range(N):
+            #threads.append(Thread(target=temporal_calculation, args=( i, N, lambda H: np.mean(np.array(list(nx.average_neighbor_degree(H).values()))) ))) #Degree
+            threads.append(Thread(target=temporal_calculation, args=( i, N, lambda H: nx.density(H) )))#np.mean(list(nx.average_neighbor_degree(H).values())) )))
+        for i in range(N):
+            threads[i].start()
+        for i in range(N):
+            threads[i].join()
+        #print(sorted(numbers))
+        np.savetxt('temporal/densidade/'+sys.argv[1]+'.csv', temporal, delimiter=',')
+        print(time.time() - start)
+    except Exception as e:
+        print ("Unable to start thread: {}".format(str(e)))
+
+"""
--- Converter.py
+++ Converter.py
@@ -3,7 +3,7 @@ from otf2.events import *
 import networkx as nx
 from itertools import permutations
 
-def create_graph(extract):
+def create_graph(extract, name):
     G = nx.MultiDiGraph()
     identifier = 0
     for link in extract:
@@ -13,7 +13,7 @@ def create_graph(extract):
             G.add_node(link['receiver'])
         G.add_edge(link['sender'], link['receiver'], start=float(link['time']), end=float(link['end']), label=str(link['sender'])+"_"+str(link['receiver']), id=identifier)
         identifier += 1
-    nx.write_gexf(G, 'out.gexf')
+    nx.write_gexf(G, name + '.gexf')
     return G
 
 def filter_links(graph):
@@ -26,27 +26,51 @@ def filter_links(graph):
         r = filter(lambda elem: elem['sender'] == s['sender'] and elem['receiver'] == s['receiver'], receives).__next__()
         receives.pop(receives.index(r))
         sends[sends.index(s)]['end'] = r['time']
-        if normalize == True:
-            normalize = r['time']
-        sends[sends.index(s)]['time'] /= normalize
-        sends[sends.index(s)]['end'] /= normalize
+        #if normalize == True:
+        #    normalize = r['time']
+        #sends[sends.index(s)]['time'] /= normalize
+        #sends[sends.index(s)]['end'] /= normalize
     return sends
 
-with otf2.reader.open("/storage/magueta-storage/n-body/BH/py/scorep_4_128@100/traces.otf2") as trace:
+with otf2.reader.open("/storage/magueta-storage/n-body/BH/py/scorep_16_128@100/traces.otf2") as trace:
     send_receives = []
     collectives = []
-    average_allreduce = 4.17*(10**(-6))
+    average_allreduce = 7.12*(10**(-6))
+    average_gatherv = 9.15*(10**(-6))
+    average_scatter = 13350.15*(10**(-6))
+    average_gather = 22410.37*(10**(-6))
+    average_broadcast = 6477.11*(10**(-6))
+    average_barrier = 1912813.09*(10**(-6))
+    OPTION_BARRIER = False
+    OPTION_ALLREDUCE = False
+
     for item in trace.events:
-        if item[1].__class__.__name__ == 'MpiSend':
-            send_receives.append({'time': item[1].time-trace.definitions.clock_properties.global_offset, 'sender': int(item[0].group.name.split(" ")[-1]), 'receiver': item[1].receiver, 'link_type': item[1].__class__.__name__})
+        if item[1].__class__.__name__ == 'MpiSend' or item[1].__class__.__name__ == 'MpiIsend':
+            send_receives.append({'time': item[1].time-trace.definitions.clock_properties.global_offset, 'sender': int(item[0].group.name.split(" ")[-1]), 'receiver': item[1].receiver, 'link_type': "MpiSend"})
         elif item[1].__class__.__name__ == 'MpiRecv':
-            send_receives.append({'time': item[1].time-trace.definitions.clock_properties.global_offset, 'sender': item[1].sender, 'receiver': int(item[0].group.name.split(" ")[-1]), 'link_type': item[1].__class__.__name__})
+            send_receives.append({'time': item[1].time-trace.definitions.clock_properties.global_offset, 'sender': item[1].sender, 'receiver': int(item[0].group.name.split(" ")[-1]), 'link_type': "MpiRecv"})
         elif item[1].__class__.__name__ == 'MpiCollectiveEnd':
             #print(item[0], item[1], item[1].communicator.group.members)
-            if item[1].collective_op == otf2.CollectiveOp.ALLREDUCE:
+            if item[1].collective_op == otf2.CollectiveOp.ALLREDUCE and OPTION_ALLREDUCE:
                 comm_permutation = permutations(map(lambda item: int(item.group.name.split(" ")[-1]), item[1].communicator.group.members), 2)
                 for c1, c2 in comm_permutation:
                     collectives.append({'time': (item[1].time-trace.definitions.clock_properties.global_offset)-average_allreduce, 'end': item[1].time-trace.definitions.clock_properties.global_offset, 'sender': c1, 'receiver': c2, 'link_type': "MPIAllReduce"})
-
+            elif item[1].collective_op == otf2.CollectiveOp.SCATTER:
+                communicator = map(lambda item: int(item.group.name.split(" ")[-1]), item[1].communicator.group.members)
+                for c in communicator:
+                    if c != int(item[0].group.name.split(" ")[-1]):
+                        collectives.append({'time': (item[1].time-trace.definitions.clock_properties.global_offset)-average_scatter, 'end': item[1].time-trace.definitions.clock_properties.global_offset, 'sender': int(item[0].group.name.split(" ")[-1]), 'receiver': c, 'link_type': "MPIScatter"})
+            elif item[1].collective_op == otf2.CollectiveOp.GATHER or item[1].collective_op == otf2.CollectiveOp.GATHERV:
+                communicator = map(lambda item: int(item.group.name.split(" ")[-1]), item[1].communicator.group.members)
+                for c in communicator:
+                    if c != int(item[0].group.name.split(" ")[-1]):
+                        if item[1].collective_op == otf2.CollectiveOp.GATHERV:
+                            collectives.append({'time': (item[1].time-trace.definitions.clock_properties.global_offset)-average_gatherv, 'end': item[1].time-trace.definitions.clock_properties.global_offset, 'sender': c, 'receiver': int(item[0].group.name.split(" ")[-1]), 'link_type': "MPIGatherv"})
+                        else:
+                            collectives.append({'time': (item[1].time-trace.definitions.clock_properties.global_offset)-average_gather, 'end': item[1].time-trace.definitions.clock_properties.global_offset, 'sender': c, 'receiver': int(item[0].group.name.split(" ")[-1]), 'link_type': "MPIGather"})
+            elif item[1].collective_op == otf2.CollectiveOp.BARRIER and OPTION_BARRIER:
+                comm_permutation = permutations(map(lambda item: int(item.group.name.split(" ")[-1]), item[1].communicator.group.members), 2)
+                for c1, c2 in comm_permutation:
+                    collectives.append({'time': (item[1].time-trace.definitions.clock_properties.global_offset)-average_barrier, 'end': item[1].time-trace.definitions.clock_properties.global_offset, 'sender': c1, 'receiver': c2, 'link_type': "MPIBarrier"})
     graph = (filter_links(sorted(send_receives, key=lambda  x: x['time'])) + collectives)
-    create_graph(graph)
+    create_graph(graph, "BH16")
